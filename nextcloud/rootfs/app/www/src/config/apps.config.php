578616107e8612143b97150a3f560689cc10ce09
fabio-garavini <info@fabiogaravini.dev>
2025-02-28 10:07:53 +0100

    nextcloud version 31.0.0

---
+++ nextcloud/rootfs/app/www/src/config/apps.config.php
@@ -0,0 +1,15 @@
+<?php
+$CONFIG = array (
+  'apps_paths' => array (
+      0 => array (
+              'path'     => '/share/nextcloud/apps',
+              'url'      => '/apps',
+              'writable' => false,
+      ),
+      1 => array (
+              'path'     => '/share/nextcloud/custom_apps',
+              'url'      => '/custom_apps',
+              'writable' => true,
+      ),
+  ),
+);
