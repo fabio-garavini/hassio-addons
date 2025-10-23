d49f65a34cca23c583998178a97371673fb8afdd
fabio-garavini <info@fabiogaravini.dev>
2025-10-23 11:04:21 +0200

    [immich_frame] add build test

--- immich_frame/rootfs/usr/local/bin/check-ssl.sh
+++ immich_frame/rootfs/usr/local/bin/check-ssl.sh
@@ -16,19 +16,15 @@ declare renew_days=90
 certfile="$1"
 keyfile="$2"
 
-if ! bashio::fs.file_exists "$certfile" || ! bashio::fs.file_exists "$keyfile"; then
-    /usr/local/bin/selfsigned-ssl-gen.sh $certfile $keyfile
-else
-    enddate=$(openssl x509 -enddate -noout -in "$certfile" 2>/dev/null || true)
-    if [ -n "$enddate" ]; then
-        expiry_date=$(echo "$enddate" | cut -d= -f2 | sed 's/ GMT$//')
-        expiry_ts=$(date -d "$expiry_date" +%s)
-        now_ts=$(date +%s)
-        days_left=$(( (expiry_ts - now_ts) / 86400 ))
+enddate=$(openssl x509 -enddate -noout -in "$certfile" 2>/dev/null || true)
+if [ -n "$enddate" ]; then
+    expiry_date=$(echo "$enddate" | cut -d= -f2 | sed 's/ GMT$//')
+    expiry_ts=$(date -d "$expiry_date" +%s)
+    now_ts=$(date +%s)
+    days_left=$(( (expiry_ts - now_ts) / 86400 ))
 
-        if [ "$days_left" -le "$renew_days" ]; then
-            bashio::log.info "Self-signed cert expiring in $days_left days, regenerating..."
-            /usr/local/bin/selfsigned-ssl-gen.sh $certfile $keyfile
-        fi
+    if [ "$days_left" -le "$renew_days" ]; then
+        bashio::log.info "Self-signed cert expiring in $days_left days, regenerating..."
+        /usr/local/bin/selfsigned-ssl-gen.sh $certfile $keyfile
     fi
 fi
--- immich_frame/rootfs/usr/local/bin/selfsigned-ssl-gen.sh
+++ immich_frame/rootfs/usr/local/bin/selfsigned-ssl-gen.sh
@@ -3,8 +3,8 @@
 
 # Check for required arguments
 if [ $# -ne 2 ]; then
-    bashio::log.error "[selfsigned-ssl-gen.sh] missing: <certfile> <keyfile>"
-    exit 1
+  bashio::log.error "[selfsigned-ssl-gen.sh] missing: <certfile> <keyfile>"
+  exit 1
 fi
 
 certfile="$1"
@@ -15,7 +15,9 @@ if [[ -d "/data/ssl" ]]; then
 fi
 
 mkdir -p /data/ssl
-hostname="$(bashio::info.hostname)"
+if ! hostname="$(bashio::info.hostname 2>/dev/null)"; then
+  hostname="homeassistant.local"
+fi
 tmp_openssl_cfg=$(mktemp)
 trap 'rm -f "$tmp_openssl_cfg"' EXIT
 
