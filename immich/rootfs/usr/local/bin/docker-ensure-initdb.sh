7016e84cd71700737b3e9f15b6179fb89d5ff10c
fabio-garavini <info@fabiogaravini.dev>
2025-11-24 19:10:27 +0100

    [immich] update postgres initialization

--- immich/rootfs/usr/local/bin/docker-ensure-initdb.sh
+++ immich/rootfs/usr/local/bin/docker-ensure-initdb.sh
@@ -33,41 +33,23 @@ fi
 
 # only run initialization on an empty data directory
 if [ -z "$DATABASE_ALREADY_EXISTS" ]; then
-	docker_verify_minimum_env
-	docker_error_old_databases
+  docker_verify_minimum_env
+  docker_error_old_databases
 
-	# check dir permissions to reduce likelihood of half-initialized database
-	ls /docker-entrypoint-initdb.d/ > /dev/null
+  # check dir permissions to reduce likelihood of half-initialized database
+  ls /docker-entrypoint-initdb.d/ > /dev/null
 
-	docker_init_database_dir
-	pg_setup_hba_conf "$@"
+  docker_init_database_dir
+  pg_setup_hba_conf "$@"
 
-	# PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
-	# e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
-	export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
-	docker_temp_server_start "$@"
+  # PGPASSWORD is required for psql when authentication is required for 'local' connections via pg_hba.conf and is otherwise harmless
+  # e.g. when '--auth=md5' or '--auth-local=md5' is used in POSTGRES_INITDB_ARGS
+  export PGPASSWORD="${PGPASSWORD:-$POSTGRES_PASSWORD}"
+  docker_temp_server_start "$@"
 
-	docker_setup_db
-	docker_process_init_files /docker-entrypoint-initdb.d/*
+  docker_setup_db
+  docker_process_init_files /docker-entrypoint-initdb.d/*
 
-	docker_temp_server_stop
-	unset PGPASSWORD
-else
-	self="$(basename "$0")"
-	case "$self" in
-		docker-ensure-initdb.sh)
-			echo >&2 "$self: note: database already initialized in '$PGDATA'!"
-			exit 0
-			;;
-
-		docker-enforce-initdb.sh)
-			echo >&2 "$self: error: (unexpected) database found in '$PGDATA'!"
-			exit 1
-			;;
-
-		*)
-			echo >&2 "$self: error: unknown file name: $self"
-			exit 99
-			;;
-	esac
+  docker_temp_server_stop
+  unset PGPASSWORD
 fi
